f75e2fc81c2f0feb0e9e7b89e86dedf2895091a4
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2014-04-23 15:04:35 -0700

    Add views support

--- test/views_test.js
+++ test/views_test.js
@@ -2,11 +2,15 @@ var assert = require('assert'),
     App = require('../lib/app').App
     Response = require('../lib/response').Response
 
-xdescribe('Views', function() {
+describe('Views', function() {
   beforeEach(function() {
     this.app = new App()
     this.app.set('views', __dirname + '/views')
     this.app.set('view engine', 'jade')
+  })
+
+  it('has settings', function(){
+    assert.equal(this.app.settings['view engine'], 'jade');
   })
 
   it('render from app', function() {
